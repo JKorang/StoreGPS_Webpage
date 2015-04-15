1cde736c1efcfda3eeeda30c41862c6f56baceba
Joseph Falcone <falcon45@students.rowan.edu>
2015-04-15 12:47:25 -0400

    another attempt

--- access.php
+++ access.php
@@ -1,44 +1,44 @@
 <?php
 
+
 $dbconn = pg_connect ("host=localhost dbname=webpagedb user=webpagedb password=");
 
+//split the table into two smaller queries one for the stores and one for the items
+$store_query = pg_query($dbconn, "SELECT data FROM web_webform_submitted_data Where nid = 9");
+$item_query = pg_query($dbconn, "SELECT data FROM web_webform_submitted_data Where nid = 10");
 
-echo "hello world";
-
-$result = pg_query($dbconn, "SELECT * FROM Edit_Store_Information");
-
-if (!$result) {
+//if one of the queries can not be accessed an error is thrown
+if (!$store_query || !$item_query) {
   echo "An error occurred.\n";
   exit;
 }
+
+
+$stores = array();
+//go through the store_query and add the data to an array
+while($storeRow = pg_fetch_array($store_query)){
   
-echo $result;
-
-
-while ($row = pg_fetch_array($result)) {
-
-echo "in loop";
-    echo json_encode($row);
+  $stores[] = $storeRow['data'];
+  //echo $storeRow['data'];
 
 }
 
-pg_free_result($result);
-
-$result = pg_query("SELECT * FROM Add_Item");
-
-if (!$result) {
-  echo "An error occurred.\n";
-  exit;
-}
-
-while ($row = pg_fetch_array($result)) {
-
-   echo json_encode($row);
+$items = array();
+//go through the item_query and add the data to an array
+while($itemRow = pg_fetch_array($item_query)){
+  
+  $items[] = $item_query['data'];
+  //echo $itemRow['data'];
 
 }
+
+//json items of the stores array and the items array
+$jsonStores = array_to_json($stores);
+
+$jsonItems = array_to_json($items);
+
+
 
 pg_close($dbconn);
-
-
 
 ?>
